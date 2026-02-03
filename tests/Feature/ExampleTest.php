d691157889e94e8e521c1cc5aa4fc6802c46a730
MagdyMahmoud88 <magdymahmoud8812@gmail.com>
2026-02-03 13:24:18 +0200

    update project

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Models\Idea;
+
 test('the application returns a successful response', function (): void {
     $response = $this->get('/');
 
     $response->assertStatus(200);
 });
+
